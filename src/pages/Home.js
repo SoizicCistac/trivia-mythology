868a6c7db521ec98721de050236f805484181edb
Soizic Cistac <soizic.cistac@gmail.com>
2021-09-25 12:23:59 +0200

    container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,17 +15,19 @@ function Home (){
             <header className="header">
                 <h3 className="headerText">Trivia Mythology</h3>
             </header>
-            <h1 className="titreHome">Test Your Knowledge Mythological Edition</h1>
-            <div className="iconeHome">
-                <img className="icone" src={couronne} alt="couronne"></img>
-                <img className="icone" src={homme} alt="homme maori"></img>
-                <img className="icone" src={izanagi} alt="izanagi"></img>
-                <img className="icone" src={masque} alt="masque"></img>
-                <img className="icone" src={thor} alt="thor"></img>
-            </div>
-            <Link to="/level">
-                <button id="buttonHome">Play</button>
-            </Link>  
+            <div className="bodyContainer">
+                <h1 className="titreHome">Test Your Knowledge Mythological Edition</h1>
+                <div className="iconeHome">
+                    <img className="icone" src={couronne} alt="couronne"></img>
+                    <img className="icone" src={homme} alt="homme maori"></img>
+                    <img className="icone" src={izanagi} alt="izanagi"></img>
+                    <img className="icone" src={masque} alt="masque"></img>
+                    <img className="icone" src={thor} alt="thor"></img>
+                </div>
+                <Link to="/level">
+                    <button id="buttonHome">Play</button>
+                </Link>
+            </div>  
             <Footer/>
         </div>
     )
