bf90d95a9571cd2ec8515d07136266424e33cf8a
marie-laure Debeaune <ml.debeaune@gmail.com>
2021-09-25 09:44:59 +0200

    add pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
 
-function Home () {
+function Home (){
+
     return(
-        <div className='homeDiv'>
-            <h1>Test Your Knowledge : Mythological Edition</h1>
-            <Link to='/Easy'>
-                <button className='selection'>Easy</button>
-                </Link>
-            <Link to='/medium'>
-                <button className='selection'>Medium</button>
-                </Link>
-            <Link to='/Hard'>
-                <button className='selection'>Hard</button>
-            </Link>
-            <Link to='/AnyDifficulty'>
-                <button className='selection'>Any Difficulty</button>
-            </Link>
+        <div>
+            <h1>Test Your Knowledge Mythological Edition</h1>
+            <Link to="/level">
+                <button id="buttonHome">Play</button>
+            </Link>  
         </div>
     )
 }
-
 export default Home;
